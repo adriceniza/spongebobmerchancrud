a689d35d670790a81f12d66e4d74a3ae61b337e5
adriceniza <ceniiziienta@gmail.com>
2021-10-08 23:40:23 +0100

    first commit

---
+++ backend/spongebobClothes/src/test/java/com/ceniza/spongebobclothes/SpongebobClothesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceniza.spongebobclothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpongebobClothesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
