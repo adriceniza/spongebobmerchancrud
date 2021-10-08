dfbd161ea1d711a59d062d522d667b7f9303cd5c
Adrián Ceniza <73916409+adriceniza@users.noreply.github.com>
2021-10-08 23:47:01 +0100

    Delete backend/spongebobClothes directory

--- backend/spongebobClothes/src/main/java/com/ceniza/spongebobclothes/entity/dao/IBasketDao.java
+++
@@ -1,7 +0,0 @@
-package com.ceniza.spongebobclothes.entity.dao;
-import org.springframework.data.repository.CrudRepository;
-import com.ceniza.spongebobclothes.entity.models.Basket;
-
-public interface IBasketDao extends CrudRepository<Basket, Long>{
-
-}
--- backend/spongebobClothes/src/main/java/com/ceniza/spongebobclothes/entity/dao/IShopDao.java
+++
@@ -1,7 +0,0 @@
-package com.ceniza.spongebobclothes.entity.dao;
-import org.springframework.data.repository.CrudRepository;
-import com.ceniza.spongebobclothes.entity.models.Clothe;
-
-public interface IShopDao extends CrudRepository<Clothe, Long>{
-
-}
