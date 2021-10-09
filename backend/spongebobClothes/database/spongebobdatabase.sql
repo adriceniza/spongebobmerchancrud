783522a6406324364b4a25ed6ac7dcb418726dcf
adriceniza <ceniiziienta@gmail.com>
2021-10-09 01:05:52 +0100

    databaseupdate

--- backend/spongebobClothes/database/spongebobdatabase.sql
+++ backend/spongebobClothes/database/spongebobdatabase.sql
@@ -1,18 +1,80 @@
-use spongebobdb;
-CREATE TABLE `spongebobclothes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `model` varchar(45) NOT NULL,
-  `prize` int NOT NULL,
-  `img` varchar(60) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: spongebobdb
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `spongebobbasket`
+--
+
+DROP TABLE IF EXISTS `spongebobbasket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `spongebobbasket` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `model` varchar(45) NOT NULL,
   `prize` int NOT NULL,
   `size` varchar(30) NOT NULL,
   `img` varchar(60) NOT NULL,
   `units` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `spongebobbasket`
+--
+
+LOCK TABLES `spongebobbasket` WRITE;
+/*!40000 ALTER TABLE `spongebobbasket` DISABLE KEYS */;
+/*!40000 ALTER TABLE `spongebobbasket` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `spongebobclothes`
+--
+
+DROP TABLE IF EXISTS `spongebobclothes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `spongebobclothes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `model` varchar(45) NOT NULL,
+  `prize` int NOT NULL,
+  `img` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `spongebobclothes`
+--
+
+LOCK TABLES `spongebobclothes` WRITE;
+/*!40000 ALTER TABLE `spongebobclothes` DISABLE KEYS */;
+INSERT INTO `spongebobclothes` VALUES (34,'SpongeBob Pack shirt and pants',24,'../../assets/images/spongebobshirtpants.jpg'),(35,'SpongeBob cap',15,'../../assets/images/spongebobcap.jfif'),(36,'Patrick costume',21,'../../assets/images/patrick.jpg'),(37,'SpongeBob Timberland boots',160,'../../assets/images/spongebobtimberland.jpg');
+/*!40000 ALTER TABLE `spongebobclothes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-10-09  0:27:57
